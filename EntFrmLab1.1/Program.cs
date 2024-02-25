997594036b049467bdd2830f136b2a6047dfaace
Easyvac16 <gdima9051@gmail.com>
2024-02-25 14:03:45 +0200

    A lot of changes

--- EntFrmLab1.1/Program.cs
+++ EntFrmLab1.1/Program.cs
@@ -29,6 +29,17 @@ namespace EntFrmLab1._1
                 context.Games.AddRange(gameInfoList);
                 context.SaveChanges();
             }*/
+            ShowData();
+            Console.WriteLine("------------------");
+            DeleteData();
+            Console.WriteLine("-----------------------");
+            ShowData();
+        }
+
+
+
+        public static void ShowData()
+        {
             using (var context = new Context())
             {
                 var games = context.Games;
@@ -39,11 +50,66 @@ namespace EntFrmLab1._1
                     {
                         game.IsMultiplayer = true;
                         game.CopiesSold = 100;
-                        context.SaveChanges();
                     }
 
                 }
 
+            }
+        }
+
+        public static GameInfo MapData()
+        {
+            GameInfo gameInfo = new GameInfo();
+
+            Console.WriteLine("Game name:");
+            gameInfo.GameName = Console.ReadLine();
+
+            Console.WriteLine("Game Genre:");
+            gameInfo.Genre = Console.ReadLine();
+
+            Console.WriteLine("Game studio:");
+            gameInfo.Studio = Console.ReadLine();
+
+            Console.WriteLine("Copies solded");
+            int.TryParse(Console.ReadLine(), out int buffer);
+            gameInfo.CopiesSold = buffer;
+
+            Console.WriteLine("Release date:");
+            DateTime.TryParse(Console.ReadLine(), out DateTime date);
+            gameInfo.ReleaseDate = date;
+
+
+
+            return gameInfo;
+        }
+
+        public static void InsertData()
+        {
+            using (var context = new Context())
+            {
+                context.Games.Add(MapData());
+                context.SaveChanges();
+            }
+        }
+
+        public static void UpdateData()
+        {
+            using (var context = new Context())
+            {
+                var game = context.Games.FirstOrDefault();
+                game.Genre = "Souls";
+                context.Games.Update(game);
+                context.SaveChanges();
+            }
+        }
+
+        public static void DeleteData() 
+        {
+            using (var context = new Context()) 
+            {
+                var game = context.Games.FirstOrDefault();
+                context.Games.Remove(game);
+                context.SaveChanges();
             }
         }
     }
